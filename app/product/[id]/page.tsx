fd117979b3183ebe2d31ac5d6b3f355186a5345f
xrhridoy1 <hmd740004@gmail.com>
2025-11-19 16:56:24 +0600

    done 4

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -1,16 +1,8 @@
 import SinglePage from '@/components/SinglePage';
 import { SingleProduct } from '@/lib/ProductFetching/Product';
-import { MainData } from '@/lib/type'
 import React from 'react';
 
 
-export async function generateStaticParams() {
-    const posts = await fetch('https://fakestoreapi.com/products').then((res) => res.json())
-
-    return posts.map((post: MainData) => ({
-        id: post.id.toString(),
-    }))
-}
 
 export async function generateMetadata({
     params,
@@ -32,13 +24,13 @@ export async function generateMetadata({
 const SignlePage = async ({
     params,
 }: {
-    params: Promise<{ id: number }>
+    params: Promise<{ id: string }>
 }) => {
 
 
     const { id } = await params
 
-    const data = await SingleProduct(id);
+    const data = await SingleProduct(Number(id));
 
     return (
         <div className='main-width '>
